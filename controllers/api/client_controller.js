b726deb66eb022f081ebefaa9065bd5cb613ae3f
Shkurenko Vira <shkurenko.verra@gmail.com>
2025-09-28 20:20:47 +0300

    feedback.controller

--- controllers/api/client_controller.js
+++ controllers/api/client_controller.js
@@ -1,42 +1,86 @@
 export default class ClientController {
+    
     doGet(request, response, id) {
-        response.writeHead(200);
-        response.end("ClientController");
+        response.writeHead(200)
+        response.end("ClientController")
     }
 
     doPost(request, response, id) {
         //Прийом тіла запиту
-        let body = "";
-        request.on('data', function (chunk) { body += chunk; });
-        request.on('end', function () {
-            console.log("ClientController::DoPost body: " + body);
-            console.log(request.headers["content-type"]);
-            if (typeof request.headers["content-type"] == 'string' &&
-                request.headers["content-type"].startsWith("application/json")) {
-                const data = JSON.parse(body); //десеріалізація - відновлення об'єкта з рядка
-                response.writeHead(200, {
-                    'Content-Type': 'application/jso'
-                });
-                response.end(JSON.stringify({
-                    "controller": "ClientController",
-                    "method": "POST",
-                    "semanthics": "Create",
-                    "body": data
-                }));
+        let body = ''
+        request.on('data', function(chunk) { body += chunk })
+        request.on('end', function() {
+            console.log('ClientController::doPost body: ' + body)
+            console.log(request.headers['content-type'])
+            if (validContentType(request.headers['content-type'])) {
+                    let data = JSON.parse(body)
+                    if (typeof data == 'object')
+                        data = JSON.stringify(data)
+                    response.writeHead(200, {
+                        'Content-Type': 'application/json'
+                    })
+                    response.end(JSON.stringify({
+                        "controller": "ClientController",
+                        "method": "POST",
+                        "semantic": "Create",
+                        "body": data
+                    }))
+            } else {
+                response.writeHead(415) // Unsupported Media Type
+                response.end()
             }
-            else {
-                response.writeHead(415);
-                response.end();
-            }
-        });
-
-
-        // response.writeHead(200, {
-        //     'Content-Type': 'application/json'
-        // });
-        // response.end(JSON.stringify({
-        //     "controller": "ClientController",
-        //     "method": "POST"
-        // }));
+        })
+        //response.writeHead(200, {
+        //    'Content-Type': 'application/json'
+        //})
+        //response.end(JSON.stringify(
+        //    {
+        //        "controller": "ClientController",
+        //        "method": "POST",
+        //        "semantic": "Create"
+        //    }))
     }
+
+    doPut(request, response, id) {
+        response.writeHead(200)
+        response.end(JSON.stringify(
+            {
+                "controller": "ClientController",
+                "method": "PUT",
+                "semantic": "Update"
+            }))
+    }
+
+    doPatch(request, response, id) {
+        response.writeHead(200)
+        response.end(JSON.stringify(
+            {
+                "controller": "ClientController",
+                "method": "PATCH",
+                "semantic": "Update"
+            }))
+    }
+
+    doDelete(request, response, id) {
+        response.writeHead(200)
+        response.end(JSON.stringify(
+            {
+                "controller": "ClientController",
+                "method": "DELETE",
+                "semantic": "Delete"
+            }))
+    }
+}
+
+function validContentType(str) {
+    if (typeof str != 'string') return false
+
+    const jsonType = 'application/json'
+    if (str.startsWith(jsonType)) {
+        if (str === jsonType || str[jsonType.length] == ';') {
+            return true
+        }
+    }
+
+    return false
 }
